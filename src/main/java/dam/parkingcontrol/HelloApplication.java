60a96a38ce863dce152dfed13c1b28221d6ad9fe
g4vr3 <gdfizan@gmail.com>
2025-02-04 09:28:29 +0100

    Add requireNonNull for stylesheet

--- src/main/java/dam/parkingcontrol/HelloApplication.java
+++ src/main/java/dam/parkingcontrol/HelloApplication.java
@@ -7,13 +7,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/dam/parkingcontrol/parking-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 700);
-        scene.getStylesheets().add(getClass().getResource("/styles/styles.css").toExternalForm());
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/styles/styles.css")).toExternalForm());
         stage.setTitle("Parking Control");
         stage.setScene(scene);
         stage.show();
