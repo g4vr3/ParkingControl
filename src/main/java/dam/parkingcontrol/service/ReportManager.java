fe159b5a833336f1d6d649af422abf383d83848a
g4vr3 <gdfizan@gmail.com>
2025-02-04 17:05:50 +0100

    Add ReportManager in service layer to manage report generation

---
+++ src/main/java/dam/parkingcontrol/service/ReportManager.java
@@ -0,0 +1,50 @@
+package dam.parkingcontrol.service;
+
+import dam.parkingcontrol.database.DatabaseConnection;
+import net.sf.jasperreports.engine.*;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Clase para gestionar la generación de reportes.
+ * Proporciona métodos estáticos para generar reportes con parámetros específicos.
+ *
+ * @version 1.0
+ * @author g4vr3
+ */
+public class ReportManager {
+
+    /**
+     * Genera un reporte diario de los registros del parking.
+     */
+    public static void generateEndOfDayReport() {
+        try {
+            String reportPath = "src/main/resources/reports/end_day_report/end_day_report.jasper";
+            String outputPath = "src/main/resources/generated-reports/" + LocalDate.now() + "_end_day_report.pdf";
+
+            // Establecer los parámetros
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("Date", Date.valueOf(LocalDate.now()));
+
+            // Obtener la conexión a la base de datos
+            try (Connection conn = DatabaseConnection.connect()) {
+                // Llenar el reporte
+                JasperPrint jasperPrint = JasperFillManager.fillReport(reportPath, parameters, conn);
+
+                // Exportar el reporte a un archivo PDF
+                JasperExportManager.exportReportToPdfFile(jasperPrint, outputPath);
+
+                // TODO: Gestionar mensajes de éxito y error en la interfaz
+                System.out.println("Reporte generado exitosamente: " + outputPath);
+            }
+        } catch (JRException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error al generar el reporte: " + e.getMessage());
+        }
+    }
+}
